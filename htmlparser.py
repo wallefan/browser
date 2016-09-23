f3b82614928ad15f5e9f4bba18bef17d3bd13ef6
wallefan <saworley39@yahoo.com>
2016-09-23 11:17:17 -0700

    Update htmlparser.py

--- htmlparser.py
+++ htmlparser.py
@@ -7,9 +7,21 @@ class Parser(html.parser.HTMLParser):
     except:
       self.current=tags.BaseTag(self.current, **dict(attrs))
   def encounter_endtag(self, tag):
-    self.current.finalize(tag)
+    self.current=self.current.finalize(tag)
   def encounter_data(self, data):
     self.current.add_data(data)
   def reset(self):
-    self.current.finalize(None) # go all the way up the tree
+    self.encounter_endtag(None) # go all the way up the tree (implicit ending tag)
     super().reset()
+  def encounter_comment(self, data):
+    if data.startswith('if'):
+      self.iffalse.append(...)
+    elif data=='else':
+      self.iffalse[-1]=not self.iffalse[-1]
+    elif data=='endif':
+      self.iffalse.pop()
+  def encounter_startendtag(self, tag, attrs):
+    # What am I supposed to do here?
+    self.encounter_starttag(tag, attrs)
+    self.encounter_endtag(tag)
+    
